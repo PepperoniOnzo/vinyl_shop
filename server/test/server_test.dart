529499fb3b45e3c743fe583ea58c3e856e5af361
PepperoniOnzo <radchukmaxym@gmail.com>
2022-12-03 12:42:28 +0200

    Vinyl&User tests

--- server/test/server_test.dart
+++ server/test/server_test.dart
@@ -1,7 +1,9 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:http/http.dart';
 import 'package:test/test.dart';
+import '../bin/models/models.dart';
 
 void main() {
   final port = '8080';
@@ -20,20 +22,66 @@ void main() {
 
   tearDown(() => p.kill());
 
-  test('Root', () async {
-    final response = await get(Uri.parse('$host/'));
-    expect(response.statusCode, 200);
-    expect(response.body, 'Hello, World!\n');
+  group('Vinyl', () {
+    test('Get vinyl', () async {
+      final response = await get(Uri.parse('$host/vinyl/0'));
+      expect(response.statusCode, 200);
+
+      final body = response.body;
+      final Vinyl vinyl = Vinyl.fromJson(jsonDecode(body));
+
+      expect(
+          vinyl,
+          Vinyl(
+            id: 0,
+            album: 'Unknown',
+            author: 'Unknown',
+            albumYear: 0,
+            plateYear: 0,
+            country: 'Unknown',
+            genre: ['Unknown'],
+            imageFull: 'Unknown',
+            imagePreview: 'Unknown',
+          ));
+    });
   });
 
-  test('Echo', () async {
-    final response = await get(Uri.parse('$host/echo/hello'));
-    expect(response.statusCode, 200);
-    expect(response.body, 'hello\n');
+  group('User', () {
+    test('Add user', () async {
+      final response = await post(Uri.parse('$host/user'),
+          headers: <String, String>{'Content-Type': 'application/json'},
+          body: jsonEncode(User(
+                  id: 0,
+                  name: 'unknown',
+                  email: 'unknown@gmail.com',
+                  password: 'unknown')
+              .toJson()));
+      expect(response.statusCode, 200);
+    });
+
+    test('Get user', () async {
+      final response = await get(Uri.parse('$host/user/0'));
+      expect(response.statusCode, 200);
+
+      final body = response.body;
+      final User user = User.fromJson(jsonDecode(body));
+      expect(
+          user,
+          User(
+              id: 0,
+              name: 'unknown',
+              email: 'unknown@gmail.com',
+              password: 'unknown'));
+    });
+
+    test('Delete user', () async {
+      final response = await delete(Uri.parse('$host/user/0'));
+      expect(response.statusCode, 200);
+    });
   });
 
   test('404', () async {
-    final response = await get(Uri.parse('$host/foobar'));
+    final response = await get(Uri.parse('$host/404foo'));
     expect(response.statusCode, 404);
   });
 }
